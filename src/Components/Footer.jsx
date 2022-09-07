2d21b9bbe1070bde0e682aed6d8db0fd36ce9bea
HassanTAli <hassantarekha@gmail.com>
2022-09-08 01:42:11 +0200

    Footer Component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={logo} alt="img" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Made With Love ❤️ And muscles 💪
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
